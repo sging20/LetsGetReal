01da4580fb5d81b034ee3a169f73ceee3424c555
sging20 <sging20@stuy.edu>
2020-11-24 22:53:59 -0500

    Updated code

--- RealNumber.java
+++ RealNumber.java
@@ -26,11 +26,4 @@ public class RealNumber extends Number {
   public String toString(){
       return ""+value;
     }
-  public boolean equals(RealNumber other){
-    if(getValue() == 0 || (getValue() == 0)){
-          return (getValue() == other.getValue());
-      }
-    if(Math.abs(getValue() - other.getValue()) <= 0.0001) return true;
-    return false;
-    }
 }
